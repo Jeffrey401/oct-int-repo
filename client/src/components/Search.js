0d969a1336e61d9eac93e17dcf69ce46764c8e6b
Alland Timas <69397969+Thunder6776@users.noreply.github.com>
2023-02-26 14:29:39 -0500

    Updated Table
    I updated the table and began implementing pagination

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,35 +1,44 @@
-import { useState } from "react"
-import data from '../converted-data.json'
+import React, { useState, useEffect } from "react";
+import data from "../converted-data.json";
 
 export default function Search() {
+  const [searchValue, setSearchValue] = useState("");
+  const [filterData, setFilterData] = useState(data);
+  const [selectedOption, setSelectedOption] = useState("");
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setFilterData(
+        data.filter(
+          (item) =>
+            item.patientId.toLowerCase().includes(searchValue.toLowerCase()) ||
+            item.firstName.toLowerCase().includes(searchValue.toLowerCase()) ||
+            item.lastName.toLowerCase().includes(searchValue.toLowerCase())
+        )
+      );
+    }, 500);
+    return () => clearTimeout(timer);
+  }, [searchValue]);
 
-    const [searchValue, setSearchValue] = useState("")
-    const [filterData, setFilterData] = useState(data)
-    const onInputChange = (e) => {
-        setSearchValue(e.target.value)
-        setFilterData(data.filter((item) => item.patientId.includes(searchValue)))
-        console.table(filterData)
-    }
+  const onInputChange = (e) => {
+    setSearchValue(e.target.value);
+  };
 
+  const handleOptionChange = (e) => {
+    setSelectedOption(e.target.value);
+  };
 
-    return (
-        <div>
-            <input 
-                value={searchValue} 
-                onChange={onInputChange}  
-                list = "searchV" 
-                type="search"  
-                className="searchExp" 
-                placeholder="Patient ID, First or LastName">
-            </input>
-            <datalist id="searchV" >
-                {
-                    filterData.map(patient =>
-                        <option  key={patient.patientId} value={patient.patientId}> {patient.patientId}</option>)
-                }
-            </datalist>
-            </div>
-          
-    )
+  return (
+    <div className="search-container">
+      <h1>Patient Search</h1>
+      <div className="search-bar">
+        <input
+          value={searchValue}
+          onChange={onInputChange}
+          type="search"
+          placeholder="Patient ID, First or LastName"
+        />
+      </div>
+    </div>
+  );
 }
