720ddb6cf2869dfd34551808f25e8a4b8730d668
Jovan Rodriguez <jovanrodriguez1998@gmail.com>
2023-03-08 09:14:37 -0500

    Putting TableRow and RowMutator in a Fragment

--- client/src/components/Admin/Table.js
+++ client/src/components/Admin/Table.js
@@ -1,5 +1,5 @@
 // import React and useState hook from React library and import ReactPaginate component and data from JSON file
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import ReactPaginate from "react-paginate";
 import './Table.css';
 import RowMutator from "./RowMutator";
@@ -16,17 +16,19 @@ export default function PatientTable() {
   const pagesVisited = pageNumber * usersPerPage;
 
   // calculate the total number of pages required for pagination based on the length of the data array and usersPerPage
-  const [pageCount, setPageCount] = useState(null); 
+  const [pageCount, setPageCount] = useState(null);
   //
 
   // function to set the data
   const [data, setData] = useState(null);
 
+  const [updatePatient, setUpdate] = useState(null);
+
   //Fetching the data from the database
   useEffect(() => {
     fetch('http://localhost:9000/exams')
       .then(response => {
-          return response.json();
+        return response.json();
       })
       .then(data => {
         console.log(data)
@@ -41,76 +43,91 @@ export default function PatientTable() {
     setPageNumber(selected);
   };
 
-  const Table = ({action}) => (
-    <table className="table text-center" >
-    <thead>
-      <tr>
-        <th>Patient ID</th>
-        <th>Age</th>
-        <th>Sex</th>
-        <th>Zipcode</th>
-        <th>BMI</th>
-        <th>Weight</th>
-        <th>ExamID</th>
-        <th>ICU Admit</th>
-        <th>ICU Num</th>
-        <th>Mortality</th>
-      </tr>
-    </thead>
-    <tbody className="adTbody">
-      {data && data
-        .slice(pagesVisited, pagesVisited + usersPerPage) // slice the data array to get the data for the current page only
-        .map((patient) => (
-          <TableRow  key={patient.patientId} patient={patient} action = {action} />
-        ))}
-    </tbody>
-  </table>
+  const Table = ({ action }) => (
+    <form>
+      <table className="table text-center" >
+        <thead>
+          <tr>
+            <th>Patient ID</th>
+            <th>Age</th>
+            <th>Sex</th>
+            <th>Zipcode</th>
+            <th>BMI</th>
+            <th>Weight</th>
+            <th>ExamID</th>
+            <th>ICU Admit</th>
+            <th>ICU Num</th>
+            <th>Mortality</th>
+          </tr>
+        </thead>
+        <tbody className="adTbody">
+          {data && data
+            .slice(pagesVisited, pagesVisited + usersPerPage) // slice the data array to get the data for the current page only
+            .map((patient) => (
+              <Fragment>
+                {updatePatient === patient.patientId ? (<RowMutator />
+                ) : ( 
+                <TableRow key={patient.patientId} patient={patient} action={action} />
+                )}
+              </Fragment>
+            ))}
+        </tbody>
+      </table>
+    </form>
 
 
   );
 
-  const TableRow = ({patient, action}) => (
-    <tr key={patient.patientId}  onClick={action(patient.patientId)} >
-    <td scope="row" >{patient.patientId}</td>
-    <td >{patient.age}</td>
-    <td>{patient.sex}</td>
-    <td>{patient.zipCode}</td>
-    <td>{patient.bmi}</td>
-    <td>{patient.weight}</td>
-    <td>{patient.examID}</td>
-    <td>{patient.icuAdmit}</td>
-    <td>{patient.icuNum}</td>
-    <td>{patient.mortality}</td>
+  const TableRow = ({ patient, action, handleEditClick }) => (
+    <tr key={patient.patientId} onClick={action(patient.patientId)} >
+      <td scope="row" >{patient.patientId}</td>
+      <td >{patient.age}</td>
+      <td>{patient.sex}</td>
+      <td>{patient.zipCode}</td>
+      <td>{patient.bmi}</td>
+      <td>{patient.weight}</td>
+      <td>{patient.examID}</td>
+      <td>{patient.icuAdmit}</td>
+      <td>{patient.icuNum}</td>
+      <td>{patient.mortality}</td>
+      <td>
+        <button
+          type="button"
+          onClick={(event) => handleEditClick(event, patient)}
+        >
+          Update
+        </button>
+      </td>
     </tr>
-    
+
   );
 
-  const handleClick = patient => (e) =>{
-    
+  const handleClick = patient => (e) => {
+
     // Patient to be update, edit or delete!
     console.log("Value of patient is " + patient);
-   
-    
+
+
   }
 
   // return a div containing the table and ReactPaginate component
   return (
     <div>
 
-        <Table data={data} action={handleClick} />
+      <Table data={data} action={handleClick} />
 
-        {/* Render the ReactPaginate component */}
-        <ReactPaginate
-          previousLabel={"< Prev"} // Set previous page button label
-          nextLabel={"Next >"} // Set next page button label
-          pageCount={pageCount} // Set the total number of pages required for pagination
-          onPageChange={changePage} // Callback function when page number is clicked
-          containerClassName={"pagination-container"} // CSS class for the pagination container
-          previousLinkClassName={"previous-page"} // CSS class for previous page button
-          nextLinkClassName={"next-page"} // CSS class for next page button
-          disabledClassName={"pagination-disabled"} // CSS class for disabled page number button
-          activeClassName={"pagination-active"} // CSS class for active page number button
-        />
+      {/* Render the ReactPaginate component */}
+      <ReactPaginate
+        previousLabel={"< Prev"} // Set previous page button label
+        nextLabel={"Next >"} // Set next page button label
+        pageCount={pageCount} // Set the total number of pages required for pagination
+        onPageChange={changePage} // Callback function when page number is clicked
+        containerClassName={"pagination-container"} // CSS class for the pagination container
+        previousLinkClassName={"previous-page"} // CSS class for previous page button
+        nextLinkClassName={"next-page"} // CSS class for next page button
+        disabledClassName={"pagination-disabled"} // CSS class for disabled page number button
+        activeClassName={"pagination-active"} // CSS class for active page number button
+      />
     </div>
   );
 }
